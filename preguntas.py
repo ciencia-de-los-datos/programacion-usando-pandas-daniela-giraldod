09c3714e2deed2383a39d92325b7d1196502cc86
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-13 11:56:58 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -39,7 +39,7 @@ def pregunta_02():
 
 def pregunta_03():
     
-    resultado = tabla0["_c1"].value_counts(sort = False)
+    resultado = tbl0["_c1"].value_counts(sort = False)
 
 
     """
@@ -87,10 +87,15 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].max()
 
 
 def pregunta_06():
+    punto6 = tbl1["_c4"].unique()
+    punto6 = punto6.tolist()
+    punto6 = sorted(list(map(lambda x: x.upper(), punto6)))
+
+    
     """
     Retorne una lista con los valores unicos de la columna _c4 de del archivo `tbl1.csv`
     en mayusculas y ordenados alfabéticamente.
@@ -99,7 +104,7 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    return
+    return punto6
 
 
 def pregunta_07():
@@ -115,7 +120,7 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].sum()
 
 
 def pregunta_08():
@@ -133,7 +138,8 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    return
+    tbl0["suma"] = tbl0["_c0"]+tbl0["_c2"]
+    return tbl0
 
 
 def pregunta_09():
@@ -151,7 +157,9 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    return
+    tbl0["year"] = tbl0["_c3"].str.split("-")
+    tbl0["year"] = tbl0["_c3"].map(lambda x: x.split("-")[0])
+    return tbl0
 
 
 def pregunta_10():
@@ -168,7 +176,10 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    return
+    tbl0 = tbl0.sort_values(["_c2"])
+    punto10 = tbl0.groupby("_c1")["_c2"].apply(lambda x:':'.join(x.astype(str))).reset_index(drop=False)
+    
+    return punto10
 
 
 def pregunta_11():
@@ -187,7 +198,10 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
-    return
+    tbl1 = tbl1.sort_values(["_c4"])
+    punto11 = tbl1.groupby("_c0")["_c4"].apply(lambda x:','.join(x.astype(str)))
+   
+    return  punto11
 
 
 def pregunta_12():
@@ -205,7 +219,11 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
-    return
+    tbl2["_c5"] = tbl2["_c5a"].map(str) + ":" + tbl2["_c5b"].map(str)
+    tbl2 = tbl2.sort_values(["_c5"])
+    punto12 = tbl2.groupby("_c0")["_c5"].apply(lambda x:','.join(x.astype(str)))
+    
+    return punto12
 
 
 def pregunta_13():
@@ -222,4 +240,9 @@ def pregunta_13():
     E    275
     Name: _c5b, dtype: int64
     """
-    return
+    tabla = pd.merge(tbl0,tbl2, on = "_c0" )
+    #tabla["suma"] = tabla["_c1"] + tabla["_c5b"]
+    tabla
+    punto13 = tabla.groupby("_c1")["_c5b"].sum()
+    
+    return punto13
